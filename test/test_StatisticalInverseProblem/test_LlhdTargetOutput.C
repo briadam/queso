4d8bb057ea53835ad533c8981f496b6e08eecdbe
Brian Adams <briadam@sandia.gov>
2015-04-15 00:59:59 -0600

    Modify test_LlhdTargetOutput to demonstrate libqueso/queso/issues/203
    Demonstrate Cholesky factorization "error", I believe during an
    adaptive Metropolis update to an initially positive definite proposal
    covariance.
    
    To reproduce:
      cd build/test
      make test_LlhdTargetOutput
      ./test_LlhdTargetOutput test_StatisticalInverseProblem/llhdout_input.txt

--- test/test_StatisticalInverseProblem/test_LlhdTargetOutput.C
+++ test/test_StatisticalInverseProblem/test_LlhdTargetOutput.C
@@ -23,8 +23,12 @@ public:
   virtual double lnValue(const V & domainVector, const V * domainDirection,
       V * gradVector, M * hessianMatrix, V * hessianEffect) const
   {
-    double misfit = 1.0;
-
+    // undo the scaling transformation
+    double x1 = 2.0*domainVector[0];
+    double x2 = 2.0*domainVector[1];
+    double resid1 = 10.0*(x2-x1*x1);
+    double resid2 = 1.0-x1;
+    double misfit = resid1*resid1 + resid2*resid2;
     return -0.5 * misfit;
   }
 
@@ -41,14 +45,16 @@ int main(int argc, char ** argv) {
 
   QUESO::FullEnvironment env(MPI_COMM_WORLD, argv[1], "", NULL);
 
+  // 2 vars
   QUESO::VectorSpace<QUESO::GslVector, QUESO::GslMatrix> paramSpace(env,
-      "param_", 1, NULL);
+      "param_", 2, NULL);
 
   QUESO::GslVector paramMins(paramSpace.zeroVector());
   QUESO::GslVector paramMaxs(paramSpace.zeroVector());
 
-  double min_val = 0.0;
-  double max_val = 1.0;
+  // Original domain [-2, 2] --> [-1, 1]
+  double min_val = -2.0/2.0;
+  double max_val = 2.0/2.0;
   paramMins.cwSet(min_val);
   paramMaxs.cwSet(max_val);
 
@@ -67,12 +73,39 @@ int main(int argc, char ** argv) {
     ip("", NULL, priorRv, lhood, postRv);
 
   QUESO::GslVector paramInitials(paramSpace.zeroVector());
-  paramInitials[0] = 0.0;
+  // Scaled initial value
+  paramInitials[0] = -0.5; 
+  paramInitials[1] = 0.5;  
 
   QUESO::GslMatrix proposalCovMatrix(paramSpace.zeroVector());
-  proposalCovMatrix(0, 0) = 0.1;
 
-  ip.solveWithBayesMetropolisHastings(NULL, paramInitials, &proposalCovMatrix);
+  proposalCovMatrix(0, 0) = 0.125;
+  proposalCovMatrix(0, 1) = -0.25;
+  proposalCovMatrix(1, 0) = -0.25;
+  proposalCovMatrix(1, 1) = 0.50125;
+
+  QUESO::MhOptionsValues* calIpMhOptionsValues = new QUESO::MhOptionsValues();
+
+  // Dakota setttings aren't needed to reproduce the error
+
+  // calIpMhOptionsValues->m_rawChainSize = 1000;
+  // calIpMhOptionsValues->m_putOutOfBoundsInChain       = false;
+
+  // calIpMhOptionsValues->m_drMaxNumExtraStages = 1;
+  // calIpMhOptionsValues->m_drScalesForExtraStages.resize(1);
+  // calIpMhOptionsValues->m_drScalesForExtraStages[0] = 5;
+
+  // calIpMhOptionsValues->m_amInitialNonAdaptInterval = 1;
+  // calIpMhOptionsValues->m_amAdaptInterval           = 100;
+  // calIpMhOptionsValues->m_amEta                     = 2.88;
+  // calIpMhOptionsValues->m_amEpsilon                 = 1.e-8;
+
+  // Occurs with or without LogitTransform
+
+  // calIpMhOptionsValues->m_doLogitTransform = true;
+
+  ip.solveWithBayesMetropolisHastings(calIpMhOptionsValues, paramInitials,
+				      &proposalCovMatrix);
 
   MPI_Finalize();
 
